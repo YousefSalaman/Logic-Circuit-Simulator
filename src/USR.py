568a2ccb982a6b2853d196f01bb872270972f862
YousefSalaman <yousefsalaman@gmail.com>
2021-05-03 21:37:41 -0400

    Updated USR component

--- src/USR.py
+++ src/USR.py
@@ -1,3 +1,6 @@
+
+from collections import deque
+
 from .base_comp import DigitalComponent
 
 
@@ -18,34 +21,25 @@ class UniversalReg(DigitalComponent):
 
       - s0 = 1 and s1 = 1: Parallel shift elements
 
-    - reg_enable and ctrl_enable: These two pins need to be enabled for the
-      register to operate.
-
-    - clear: If this pin is enabled, then the register will get cleared. That
-      is, the register will be now full of 0's.
+    - clk: This pin must be 1 for the register to output a value.
     """
 
-    def __init__(self, name, size=None, load_input=None, init_state=0):
+    _REG_SIZE = 4  # Size of register
+
+    def __init__(self, name, load_input=None, init_state=0):
 
         super().__init__(name, init_state)
-        self.prev_state = (0, 0)
-        if load_input:
-            self.reg_bits = _RegisterBits(load_input)
-        elif size:
-            self.reg_bits = _RegisterBits([None] * size)
+        if load_input is None:
+            self.reg_bits = deque([None] * self._REG_SIZE)
         else:
-            raise ValueError('Either "size" or "load_input" have to be specified to create a Register object.')
+            if len(load_input) != 4:
+                raise ValueError("The length of the 'load_input' has to be 4")
+            self.reg_bits = deque(load_input)
 
     def print(self):
 
         run_str = super().print().replace("\n\n", "")
-        if self.prev_state == (1, 0):
-            self.reg_bits.reverse()
-            run_str += f'; Current registry: {self.reg_bits}'
-            self.reg_bits.reverse()
-        else:
-            run_str += f'; Current registry: {self.reg_bits}'
-        return run_str + '\n\n'
+        return run_str + f'; Current registry: {list(self.reg_bits)}\n\n'
 
     def verify(self, inputs):
 
@@ -55,71 +49,17 @@ class UniversalReg(DigitalComponent):
 
     def run(self, inputs):
 
-        s0, s1, reg_enable, crtl_enable, clear, *reg_inputs = inputs
+        s0, s1, clk, *reg_inputs = inputs
+        if len(reg_inputs) == 0:
+            reg_inputs = [None] * self._REG_SIZE
 
-        if clear:  # If the clear input is enabled, then the register resets its internal storage
-            self.reg_bits.clear()
-        if reg_enable == crtl_enable == 1:  # If the reg_inputs below are active, then the register will change
-            self._register_outputs(s0, s1, reg_inputs)
-            curr_state = (s0, s1)
-            if curr_state != (0, 0):
-                self.prev_state = curr_state
-
-    def _register_outputs(self, s0, s1, reg_inputs):
-        """
-        In here, you will find the possible outputs of the register.
-        """
-
-        self._reverse_register_bits((s0, s1))
-
-        if s0 != s1:
-            self.output = self.reg_bits.shift_serial(reg_inputs)
-        elif s0 and s1:
-            self.output = self.reg_bits.shift_parallel(reg_inputs)
-
-    def _reverse_register_bits(self, curr_state):
-        """
-        This reverses the register bits before applying the appropriate
-        translation with RegisterBits methods.
-        """
-
-        if self.prev_state != curr_state and (curr_state == (1, 0) or self.prev_state == (1, 0)):
-            self.reg_bits.reverse()
-
-
-class _RegisterBits(list):
-    """
-    Helper class for register.
-
-    This extends some of the functionalities of the list class, so it behaves
-    like internal registry of a shift register
-    """
-
-    def __init__(self, register_list):
-
-        self.reg_size = len(register_list)
-        super().__init__(register_list)
-
-    def shift_serial(self, list_input):
-
-        if not list_input:
-            list_input = [None]
-        last_item = self.pop(self.reg_size - 1)
-        self.insert(0, list_input[0])
-        return last_item
-
-    def shift_parallel(self, new_list):
-
-        if len(new_list) == 0:
-            new_list = self.reg_size * [None]  # Default parameter
-
-        if len(new_list) == self.reg_size:
-            prev_reg_bits = self
-            self[:] = new_list
-            return prev_reg_bits
-
-        raise Exception('The input list has to be the same size as the list within the object.')
-
-    def clear(self):
-
-        self[:] = [0] * self.reg_size
+        if clk == 1:  # If clk is one, then the register will output values
+            if not s0 and s1:
+                self.output = self.reg_bits.popleft()
+                self.reg_bits.append(reg_inputs[0])
+            if s0 and not s1:
+                self.output = self.reg_bits.pop()
+                self.reg_bits.appendleft(reg_inputs[0])
+            elif s0 == s1:  # Parallel load
+                self.output = self.reg_bits
+                self[:] = reg_inputs
